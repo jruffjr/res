e4dc638214aa4ea3aba3eadf48d45c847aab5326
jruffjr <54455028+jruffjr@users.noreply.github.com>
2019-10-23 19:51:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("#aboutme").addEventListener("click", function(){
+
+});
+// document.querySelector("h1").classList.toggle("color");
+document.querySelector("#contactme").addEventListener("click", function(){
+
+});
